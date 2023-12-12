7ad3082116cb2a648b3a4a6795b6d7ee051f5155
WebDevelopo11 <si.sada93@gmail.com>
2023-12-12 13:19:31 +0100

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const filteredPersons = filter === '' ? [...persons] : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,7 +25,7 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const areInputValid = () => newName !== '' & newNumber !== ''
+  const areInputValid = () => newName !== '' && newNumber !== ''
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -28,7 +38,8 @@ const App = () => {
 
     const newPerson = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
@@ -38,6 +49,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -46,11 +62,14 @@ const App = () => {
           number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
-          <button type="submit" disabled={!areInputValid}>add</button>
+          <button type="submit" disabled={!areInputValid()}>add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {filteredPersons.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
+      </ul>
     </div>
   )
 }
